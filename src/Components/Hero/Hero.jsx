10271bb72ba8ff9799497b34da6ffc5ebbfa6bd7
Omar Yousef <omaryousef6696@gmail.com>
2024-10-28 14:14:45 +0300

    Cusomt

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,33 +1,36 @@
 import "./Hero.css";
-import developerAnimation from '../../animation/developer.animation/Animation - 1720963041870.json'
+import developerAnimation from "../../animation/developer.animation/Animation - 1720963041870.json";
 import Lottie from "lottie-react";
 import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
+import facebook from "../../assets/img/facebook.png";
+import whatsapp from "../../assets/img/whatsap.png";
+import gmail from "../../assets/img/Gmail.png";
+import linkdin from "../../assets/img/linkdin.png";
 
 export default function Hero() {
   return (
-    <motion.section className="hero flex"
-    initial={{opacity : 0 }}
-    animate={{opacity : 1 }}
-    transition={{duration : 0.3}}
+    <motion.section
+      className="hero flex"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.3 }}
     >
       <div className="left-section">
-        <h1 className="title"
-        
-        >
-        I'm
-        <span className="my-name"> Omar Youssef</span>, 
+        <h1 className="title">
+          I'm
+          <span className="my-name"> Omar Youssef</span>,
         </h1>
-        <motion.p className="sub-title"
-          initial={{ scale: 0.6}}
-          animate={{ scale: 1}}
-          transition={{duration : 0.6}}
+        <motion.p
+          className="sub-title"
+          initial={{ scale: 0.6 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.6 }}
         >
-          Front-End developer specializing in
-          creating outstanding and functional user experiences. With my
-          expertise in modern web technologies such as HTML5, CSS3, and
-          JavaScript, along with libraries and frameworks like React.js, I can
-          transform complex ideas into simple and attractive user interfaces.
+          Front-End developer specializing in creating outstanding and
+          functional user experiences. With my expertise in modern web
+          technologies such as HTML5, CSS3, and JavaScript, along with libraries
+          and frameworks like React.js, I can transform complex ideas into
+          simple and attractive user interfaces.
           <br />
           My goal is to deliver innovative and efficient tech solutions that
           help businesses and individuals achieve their digital goals. If you
@@ -35,15 +38,31 @@ export default function Hero() {
           to help you realize your vision.
         </motion.p>
         <div className="all-icons flex">
-          <Link to="https://www.facebook.com/omar.bombony"  target="_blank" className="icon icon-facebook"/>
-          <Link to="https://wa.me/201141214658" target="_blank" className="icon icon-whatsapp"/>
-          <Link to="#contact-us" className="icon icon-gmail"/>
-          <Link to="https://www.linkedin.com/in/omar-shahen/"  target="_blank" className="icon icon-linkedin2"/>
+          <a
+            href="https://www.facebook.com/omar.bombony"
+            target="_blank"
+            className="icon"
+          >
+            <img src={facebook} className="w-100" alt="" />
+          </a>
+          <a href="https://wa.me/201141214658" target="_blank" className="icon">
+            <img src={whatsapp} className="w-100 pt-1" alt="" />
+          </a>
+          <a href="#contact-us" className="icon ">
+            <img src={gmail} className="w-100" alt="" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/omar-shahen/"
+            target="_blank"
+            className="icon"
+          >
+            <img src={linkdin} className="w-75" alt="" />
+          </a>
         </div>
       </div>
 
       <div className="right-section animation">
-        <Lottie  className="developer" animationData={developerAnimation}/>
+        <Lottie className="developer" animationData={developerAnimation} />
       </div>
     </motion.section>
   );
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -3,20 +3,25 @@ import css from "../../assets/img/css.png";
 import js from "../../assets/img/js.png";
 import react from "../../assets/img/react.png";
 import redux from "../../assets/img/redux.png";
+import api from "../../assets/img/API.png";
+import bootstrap from "../../assets/img/bootstrap.png";
+import tailwind from "../../assets/img/tailwind.png";
 
-import { motion } from "framer-motion";
 import SkillBar from "./SkillBar";
 
 function Skills() {
   const skills = [
     { image: html, alt: "html-img", level: 90, name: "HTML" },
     { image: css, alt: "css-img", level: 95, name: "CSS" },
+    { image: bootstrap, alt: "bootstrap-img", level: 95, name: "bootstrap" },
     { image: js, alt: "js-img", level: 90, name: "JavaScript" },
+    { image: api, alt: "api-img", level: 90, name: "API" },
     { image: react, alt: "react-img", level: 95, name: "React" },
+    { image: tailwind, alt: "tailwind-img", level: 95, name: "tailwind" },
     { image: redux, alt: "redux-img", level: 70, name: "Redux" },
   ];
   return (
-    <div className="container bg-light bg-opacity-25 p-5  vh-100 rounded-5 ">
+    <div className="container bg-light bg-opacity-25 p-5  rounded-5 ">
       {skills.map((skill, index) => (
         <SkillBar
           key={index}
